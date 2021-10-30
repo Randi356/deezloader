bbc7b54e89b498361756f3c119dc6695d2ce6c56
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-10-30 23:54:29 +0700

    Initial commit

---
+++ deezloader/others_settings.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+answers = ["Y", "y", "Yes", "YES"]
+spotify_client_id = "5f573c9620494bae87890c0f08a60293"
+spotify_client_secret = "212476d9b0f3472eaa762d90b19b0ba8"
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="deezloader",
+    version="2021.05.07",
+    description="Downloads songs, albums or playlists from deezer",
+    license="CC BY-NC-SA 4.0",
+    author="An0nimia",
+    author_email="An0nimia@protonmail.com",
+    url="https://github.com/An0nimia/deezloader",
+    packages=["deezloader"],
+    install_requires=[
+        "Flask",
+        "flask-cors",
+        "stagger",
+        "mutagen",
+        "pycryptodome",
+        "requests",
+        "spotipy",
+        "tqdm",
+    ],
+    scripts=["deezloader/bin/deez-dw.py", "deezloader/bin/deez-web.py"],
+)
